d70052fddf1ea3fcc3fd7064b1a8a6d40b1c95be
Grant Perry <grant42perry@gmail.com>
2019-07-04 23:39:30 +1000

    Correct some buttons

--- app/src/main/java/com/normorovers/mmt/app/event/mmtevent/HomeFragment.kt
+++ app/src/main/java/com/normorovers/mmt/app/event/mmtevent/HomeFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.normorovers.mmt.app.event.mmtevent.view.team.TeamsFragment
+import com.normorovers.mmt.app.event.mmtevent.view.ticket.TicketsFragment
 import kotlinx.android.synthetic.main.fragment_home.*
 
 class HomeFragment : Fragment() {
@@ -18,13 +19,14 @@ class HomeFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        activity?.actionBar?.title = "All Teams"
         setButtonActions()
     }
 
     fun setButtonActions() {
         // Check in teams button opens fragment for checking in teams.
-        (checkin_teams as Button).setOnClickListener {
+        (list_tickets as Button).setOnClickListener {
+            activity?.supportFragmentManager?.beginTransaction()?.replace(R.id.fragment_container,
+                    TicketsFragment(null))?.addToBackStack(null)?.commit()
         }
 
         // List tickets button opens fragment for showing the list of teams.
